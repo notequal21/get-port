061de37a2fc9acb337d709c8643ea1e42d0da0e5
Kirill Makhnev <74978665+notequal21@users.noreply.github.com>
2022-03-24 10:26:06 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+
+if (document.querySelector('.burger__btn')) {
+  const openBtn = document.querySelector('.burger__btn')
+  const menu = document.querySelector('.header-body__btns')
+
+  let toggleBurger = () => {
+    if (openBtn.classList.contains('active')) {
+      openBtn.classList.remove('active')
+      menu.classList.remove('active')
+    } else {
+      openBtn.classList.add('active')
+      menu.classList.add('active')
+    }
+  }
+
+  openBtn.addEventListener('click', toggleBurger)
+}
+
+if (document.querySelector('.modal-open-btn')) {
+  const openBtn = document.querySelectorAll('.modal-open-btn')
+  const modal = document.querySelector('.contactus')
+  const modalBg = document.querySelector('.contactus__bg')
+  const body = document.querySelector('body')
+
+  let toggleModal = (e) => {
+    e.preventDefault()
+    if (modal.classList.contains('active')) {
+      modal.classList.remove('active')
+      body.classList.remove('lock')
+    } else {
+      modal.classList.add('active')
+      body.classList.add('lock')
+    }
+  }
+
+  openBtn.forEach((item) => {
+    item.addEventListener('click', toggleModal)
+  })
+  modalBg.addEventListener('click', toggleModal)
+}
+
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
