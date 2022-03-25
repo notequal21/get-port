44361b114c8bf642121d0dbb7d6c5ca898926607
Kirill Makhnev <74978665+notequal21@users.noreply.github.com>
2022-03-25 13:54:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,15 +22,34 @@ if (document.querySelector('.modal-open-btn')) {
   const modal = document.querySelector('.contactus')
   const modalBg = document.querySelector('.contactus__bg')
   const body = document.querySelector('body')
+  const content = document.querySelectorAll('.container')
 
   let toggleModal = (e) => {
     e.preventDefault()
+
+    let div = document.createElement('div');
+    div.style.overflowY = 'scroll';
+    div.style.width = '50px';
+    div.style.height = '50px';
+    document.body.append(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+
+    div.remove();
+
     if (modal.classList.contains('active')) {
       modal.classList.remove('active')
       body.classList.remove('lock')
+      content.forEach((item) => {
+        item.style.maxWidth = `1320px`
+        item.style.padding = ` 0 20px`
+      })
     } else {
       modal.classList.add('active')
       body.classList.add('lock')
+      content.forEach((item) => {
+        item.style.maxWidth = `${1320 + scrollWidth}px`
+        item.style.padding = ` 0 ${scrollWidth + 20}px 0 20px`
+      })
     }
   }
 
